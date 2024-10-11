c1f485f4a8ec71ee4b339797e48bff33aa7ef51b
Andrés Bonilla <abraham.go@outlook.com>
2024-10-11 16:18:11 -0600

    Add domain frameworks

---
+++ Domain/WeatherEntities/WeatherEntities/WeatherEntities.h
@@ -0,0 +1,18 @@
+//
+//  WeatherEntities.h
+//  WeatherEntities
+//
+//  Created by Andrés Bonilla Gómez on 11/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherEntities.
+FOUNDATION_EXPORT double WeatherEntitiesVersionNumber;
+
+//! Project version string for WeatherEntities.
+FOUNDATION_EXPORT const unsigned char WeatherEntitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherEntities/PublicHeader.h>
+
+
---
+++ Domain/WeatherUsecases/WeatherUsecases/WeatherUsecases.h
@@ -0,0 +1,18 @@
+//
+//  WeatherUsecases.h
+//  WeatherUsecases
+//
+//  Created by Andrés Bonilla Gómez on 11/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherUsecases.
+FOUNDATION_EXPORT double WeatherUsecasesVersionNumber;
+
+//! Project version string for WeatherUsecases.
+FOUNDATION_EXPORT const unsigned char WeatherUsecasesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherUsecases/PublicHeader.h>
+
+
