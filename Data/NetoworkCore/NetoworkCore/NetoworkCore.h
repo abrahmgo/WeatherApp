202f8993f5a1faa24de5bc01fdffe28008094838
Andrés Bonilla <abraham.go@outlook.com>
2024-10-11 16:21:46 -0600

    Add data frameworks

---
+++ Data/NetoworkCore/NetoworkCore/NetoworkCore.h
@@ -0,0 +1,18 @@
+//
+//  NetoworkCore.h
+//  NetoworkCore
+//
+//  Created by Andrés Bonilla Gómez on 11/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetoworkCore.
+FOUNDATION_EXPORT double NetoworkCoreVersionNumber;
+
+//! Project version string for NetoworkCore.
+FOUNDATION_EXPORT const unsigned char NetoworkCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetoworkCore/PublicHeader.h>
+
+
---
+++ Data/WeatherCore/WeatherCore/WeatherCore.h
@@ -0,0 +1,18 @@
+//
+//  WeatherCore.h
+//  WeatherCore
+//
+//  Created by Andrés Bonilla Gómez on 11/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherCore.
+FOUNDATION_EXPORT double WeatherCoreVersionNumber;
+
+//! Project version string for WeatherCore.
+FOUNDATION_EXPORT const unsigned char WeatherCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherCore/PublicHeader.h>
+
+
